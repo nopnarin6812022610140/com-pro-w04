b9235af859b1d2176d80e191c35ac07b1d45d738
nopnarin6812022610140 <s6812022610140@email.kmutnb.ac.th>
2025-07-14 17:32:08 +0700

    Add files via upload

---
+++ coding-w04-04-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int score = 90.5;  // มีการแปลงค่าจาก float เป็น int โดยอัตโนมัติ
+    printf("Score (int) = %d\n", score);
+    return 0;
+}
---
+++ coding-w04-04-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    float score = 90.5;
+    printf("Score (float) = %.1f\n", score);
+    return 0;
+}
